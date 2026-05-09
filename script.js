7975ab3fb296ba59fb8ca636a03787bb8b3cc1ed
ndaruruseno-2026 <ndaruruseno@gmail.com>
2026-05-09 11:08:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // =========================================
 // URL API untuk proses prediksi AI
 // =========================================
-const url = "";
+const url = "https://predict-69feb0bb42e1209faab4-dproatj77a-et.a.run.app/predict";
 
 // API Key untuk autentikasi
-const apiKey = "";
+const apiKey = ""ul_4bcf42a302b969a7fd55ecdde15b059cbe31d6e0";
 
 
 // =========================================
